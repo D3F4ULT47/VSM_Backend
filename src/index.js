98a85356395ed2b577e587dcca17af264210aefc
BlueBerryBug <24je0220@iitism.ac.in>
2025-12-02 17:16:42 +0530

    three tym

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+import {DB_NAME} from './constants.js'
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path:"./env"
+})
+
